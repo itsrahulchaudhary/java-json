0650356723777106afa39fa2f9ff9ff5f6e9e11c
itsrahulchaudhary <rahulcy49@gmail.com>
2024-06-22 11:55:06 +0530

    initial commit

---
+++ src/main/java/com/rahul/java_json/JavaJsonApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.java_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/java_json/JavaJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.java_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
